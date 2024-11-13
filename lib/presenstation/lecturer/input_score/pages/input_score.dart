97a134eaa0554575b19bd475fea41cf28974c9e1
zaki aries <zakip2528@gmail.com>
2024-11-13 11:54:54 +0700

    Update input_score.dart
    menambah nilai industri

--- lib/presenstation/lecturer/input_score/pages/input_score.dart
+++ lib/presenstation/lecturer/input_score/pages/input_score.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sistem_magang/core/config/themes/app_color.dart';
 import 'package:sistem_magang/presenstation/lecturer/input_score/widgets/add_industry_button.dart';
 import 'package:sistem_magang/presenstation/lecturer/input_score/widgets/expandable_section.dart';
-// import 'package:sistem_magang/presenstation/lecturer/input_score/widgets/industry_score_card.dart';
-// import 'package:sistem_magang/domain/entities/industry_score.dart';
-// import 'package:sistem_magang/domain/usecases/update_scores.dart';
 
 class InputScorePage extends StatefulWidget {
-  // final UpdateScoresUseCase updateScoresUseCase;
-
-  const InputScorePage({Key? key}) : super(key: key);
+  const InputScorePage({super.key});
 
   @override
   _InputScorePageState createState() => _InputScorePageState();
 }
 
 class _InputScorePageState extends State<InputScorePage> {
-  // final List<IndustryScore> _industryScores = [];
+  List<IndustryScore> _industryScores = [];
+
+  void _addIndustryScore() {
+    setState(() {
+      _industryScores.add(IndustryScore(
+        title: 'Nilai Industri ${_industryScores.length + 1}',
+        companyName: '',
+        startDate: null,
+        endDate: null,
+        score: '',
+      ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +45,7 @@ class _InputScorePageState extends State<InputScorePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ExpandableSection(
+              const ExpandableSection(
                 title: 'Proposal',
                 fields: [
                   'Tujuan Sasaran',
@@ -46,19 +54,17 @@ class _InputScorePageState extends State<InputScorePage> {
                 ],
               ),
               const SizedBox(height: 16),
-              ExpandableSection(
+              const ExpandableSection(
                 title: 'Laporan',
                 fields: ['Sistematika Penulisan', 'Bahasa', 'Isi'],
               ),
               const SizedBox(height: 16),
-              // ..._industryScores.map((score) => IndustryScoreCard(
-              //       score: score,
-              //       onRemove: () _removeIndustryScore(score),
-              //     )),
+              ..._industryScores.map((score) => IndustryScoreCard(
+                    score: score,
+                    onRemove: () => _removeIndustryScore(score),
+                  )),
               const SizedBox(height: 16),
-              AddIndustryButton(onTap: () {
-                // _addIndustryScore
-              }),
+              AddIndustryButton(onTap: _addIndustryScore),
               const SizedBox(height: 32.0),
               SizedBox(
                 width: double.infinity,
@@ -86,33 +92,178 @@ class _InputScorePageState extends State<InputScorePage> {
     );
   }
 
-  // void _addIndustryScore() {
-  //   setState(() {
-  //     _industryScores.add(IndustryScore(
-  //       title: 'Nilai Industri ${_industryScores.length + 1}',
-  //       startDate: '',
-  //       endDate: '',
-  //       score: '',
-  //     ));
-  //   });
-  // }
+  void _removeIndustryScore(IndustryScore score) {
+    setState(() {
+      _industryScores.remove(score);
+    });
+  }
+}
 
-  // void _removeIndustryScore(IndustryScore score) {
-  //   setState(() {
-  //     _industryScores.remove(score);
-  //   });
-  // }
+class IndustryScoreCard extends StatelessWidget {
+  final IndustryScore score;
+  final VoidCallback onRemove;
 
-  // void _onSubmitNilai() async {
-  //   try {
-  //     await widget.updateScoresUseCase.execute(_industryScores);
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text('Scores updated successfully')),
-  //     );
-  //   } catch (e) {
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text('Failed to update scores')),
-  //     );
-  //   }
-  // }
+  const IndustryScoreCard({
+    Key? key,
+    required this.score,
+    required this.onRemove,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          elevation: 1,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const Icon(Icons.school, color: Colors.grey),
+                    const SizedBox(width: 8),
+                    Text(
+                      score.title,
+                      style: const TextStyle(color: Colors.grey, fontSize: 16),
+                    ),
+                  ],
+                ),
+                const Icon(Icons.add_circle_outline, color: Colors.grey),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Card(
+          elevation: 1,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: score.companyName,
+                        onChanged: (value) {
+                          score.companyName = value;
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Nama Perusahaan',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon:
+                          const Icon(Icons.delete_outline, color: Colors.grey),
+                      onPressed: onRemove,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                _buildDateField(
+                  context,
+                  label: "Tanggal Mulai",
+                  value: score.startDate,
+                  onChanged: (val) => score.startDate = val,
+                ),
+                const SizedBox(height: 16),
+                _buildDateField(
+                  context,
+                  label: "Tanggal Selesai",
+                  value: score.endDate,
+                  onChanged: (val) => score.endDate = val,
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  initialValue: score.score,
+                  onChanged: (value) {
+                    // Ensuring only numbers are allowed
+                    if (value.isNotEmpty && double.tryParse(value) != null) {
+                      score.score = value;
+                    }
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Nilai',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDateField(
+    BuildContext context, {
+    required String label,
+    required DateTime? value,
+    required Function(DateTime) onChanged,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: const TextStyle(fontSize: 14, color: Colors.grey)),
+        const SizedBox(height: 8),
+        GestureDetector(
+          onTap: () async {
+            final pickedDate = await showDatePicker(
+              context: context,
+              initialDate: value ?? DateTime.now(),
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2100),
+            );
+            if (pickedDate != null) {
+              onChanged(pickedDate);
+            }
+          },
+          child: AbsorbPointer(
+            child: TextFormField(
+              controller: TextEditingController(
+                text:
+                    value != null ? DateFormat('dd-MM-yyyy').format(value) : '',
+              ),
+              decoration: InputDecoration(
+                hintText: 'Pilih Tanggal',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.grey),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IndustryScore {
+  final String title;
+  String companyName;
+  DateTime? startDate;
+  DateTime? endDate;
+  String score;
+
+  IndustryScore({
+    required this.title,
+    this.companyName = '',
+    this.startDate,
+    this.endDate,
+    this.score = '',
+  });
 }
