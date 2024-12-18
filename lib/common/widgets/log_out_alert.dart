b14625021a103baf22e2ab2646bd872e2cf1915c
Yohanes Kevin <yohanes123kevin123@gmail.com>
2024-12-18 19:16:36 +0700

    fix - alert logout

--- lib/common/widgets/log_out_alert.dart
+++ lib/common/widgets/log_out_alert.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:Sitama/common/widgets/alert.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:Sitama/common/bloc/button/button_state.dart';
 import 'package:Sitama/common/bloc/button/button_state_cubit.dart';
-import 'package:Sitama/common/widgets/basic_app_button.dart';
 import 'package:Sitama/core/config/themes/app_color.dart';
 import 'package:Sitama/domain/usecases/general/log_out.dart';
 import 'package:Sitama/presenstation/general/welcome/pages/welcome.dart';
@@ -27,39 +25,26 @@ class LogOutAlert extends StatelessWidget {
             );
           }
           if (state is ButtonFailurState) {
-            print(state.errorMessage);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.errorMessage)),
+            );
           }
         },
-        child: Builder(
-          builder: (context) => AlertDialog(
-            content: const Text(
-              'Apakah anda yakin ingin meninggalkan akun ini ?',
-            ),
-            actions: [
-              Expanded(
-                child: OutlinedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: AppColors.lightPrimary),
-                  ),
-                  child: const Text(
-                    'Cancel',
-                  ),
-                ),
-              ),
-              BasicAppButton(
-                onPressed: () {
-                  final buttonCubit =
-                      BlocProvider.of<ButtonStateCubit>(context);
-                  buttonCubit.excute(usecase: sl<LogoutUseCase>());
-                },
-                title: 'Log Out',
-                height: false,
-              )
-            ],
-          ),
+        child: CustomAlertDialog(
+          title: 'Konfirmasi Logout',
+          message: 'Apakah Anda yakin ingin keluar dari akun ini?',
+          cancelText: 'Batal',
+          confirmText: 'Keluar',
+          confirmColor: AppColors.lightPrimary, 
+          icon: Icons.logout_outlined, 
+          iconColor: AppColors.lightPrimary,
+          onCancel: () {
+            Navigator.of(context).pop();
+          },
+          onConfirm: () {
+            final buttonCubit = context.read<ButtonStateCubit>();
+            buttonCubit.excute(usecase: sl<LogoutUseCase>());
+          },
         ),
       ),
     );
