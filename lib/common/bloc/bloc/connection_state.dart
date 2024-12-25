5ee136211b6722ccb9bd1ddc3436a2ba78d9d739
Yohanes Kevin <yohanes123kevin123@gmail.com>
2024-12-25 22:46:01 +0700

    WIP : offline mode student

---
+++ lib/common/bloc/bloc/connection_state.dart
@@ -0,0 +1,13 @@
+// abstract class NetworkState {}
+
+// class NetworkInitial extends NetworkState {}
+
+// class NetworkSuccess extends NetworkState {}
+
+// class NetworkFailure extends NetworkState {
+//   final String message;
+
+//   NetworkFailure(this.message);
+// }
+
+// class NetworkLoading extends NetworkState {}
